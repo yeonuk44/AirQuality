8730385ae984f457c9e49a55af1a55a8bfbd7d5d
yeonuk44 <yeonuk44@gmail.com>
2023-07-26 18:50:23 +0900

    Initial commit

---
+++ app/src/main/java/com/chamber/airquality/MainActivity.kt
@@ -0,0 +1,11 @@
+ package com.chamber.airquality
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
